4db86fa79bfebfd0027a7e5c87bb7e26402ebd94
LisandroCorrales <urielcorrales03@gmail.com>
2024-03-04 05:04:35 -0300

    add footer, style and home page content

---
+++ funciones.js
@@ -0,0 +1,62 @@
+export function crearPelicula(movie){
+    return `
+    <article class="flex flex-col gap-3 w-10/12 md:w-5/12 xl:w-3/12 rounded-lg border border-[#701a75] bg-[#D2CCFF]">
+        <img class="w-full rounded-t-lg" src="${movie.image}" alt="" >
+        <h2 class="font-bold text-2xl font-serif text-center">${movie.title}</h2>
+        <h3 class="italic font-medium px-3">${movie.tagline}</h3>
+        <p class="px-3 pb-4 ">${movie.overview}</p>
+        <a class="flex justify-end p-2 font-medium text-indigo-800 underline" href="./details.html?id=${movie.id}">See more</a>
+    </article>
+    `;
+}
+
+export function imprimirPeliculas(listaPeliculas, elemento){
+    let template = ""
+    for (const movieIterado of listaPeliculas) {
+        template += crearPelicula(movieIterado)
+    }
+    if(listaPeliculas.length == 0){
+        template = `<h2 class="font-semibold text-2xl">There are no matching results</h2>`
+    }
+    elemento.innerHTML = template
+}
+
+export function crearLista(genero){
+    return `<option value="${genero}">
+                    ${genero}     
+            </option>`
+}
+
+
+export function filtrarPeliculasPorNombre(listaPeliculas, nombre){
+    return listaPeliculas.filter (pelicula => pelicula.title.toLowerCase().startsWith(nombre.toLowerCase()))
+}
+
+export function filtrarPeliculasPorGenero(listaPeliculas, seleccionados){
+    if(seleccionados.length == 0) {
+        return listaPeliculas
+    } else {
+        return listaPeliculas.filter( pelicula => seleccionados.some(seleccionado => pelicula.genres.includes (seleccionado)))
+    } 
+}
+
+export function verifiqueChecked() {
+    const elementosSeleccionados = [];
+    const opcionesSeleccionadas = document.querySelectorAll('option:checked');
+    
+    opcionesSeleccionadas.forEach(opcion => {
+        elementosSeleccionados.push(opcion.value);
+    });
+
+    return elementosSeleccionados;
+}
+export const fnReduce = (template, genero) => template + crearLista(genero)
+
+export function obtenerGenres(movies){
+    const generos = movies.map( movie => movie.genres).flat()  //tags
+    const setGenres = new Set(generos);
+    const generosSinRepetidos = (Array.from(setGenres)).sort();
+    generosSinRepetidos.unshift('All');
+
+    return generosSinRepetidos
+}
--- main.js
+++ main.js
@@ -1,122 +1,46 @@
-/*
-<article>
-    <img src="https://moviestack.onrender.com/static/y5szbv8zju.jpg" alt="" >
-    <h2>"The Nun II"</h2>
-    <h3>"Confess your sins."</h3>
-    <p>"In 1956 France, a priest is violently murdered, and Sister Irene begins to investigate. She once again comes face-to-face with a powerful evil."</p>
-</article>
-*/
+import { 
+    imprimirPeliculas,
+    filtrarPeliculasPorNombre, 
+    filtrarPeliculasPorGenero, 
+    verifiqueChecked, 
+    fnReduce,
+    obtenerGenres
+} from "./funciones.js"
+
+const $main = document.getElementById("contenedor-main")
 
 
 
-const main = document.getElementById("contenedor-main")
-
-function crearPelicula(movie){
-    return `
-    <article class="flex flex-col gap-3 w-10/12 md:w-5/12 xl:w-3/12 rounded-lg border border-[#701a75] bg-[#D2CCFF]">
-        <img class="w-full rounded-t-lg" src="${movie.image}" alt="" >
-        <h2 class="font-bold text-2xl font-serif text-center">${movie.title}</h2>
-        <h3 class="italic font-medium px-3">${movie.tagline}</h3>
-        <p class="px-3 pb-4 ">${movie.overview}</p>
-    </article>
-    `;
-}
-
-function imprimirPeliculas(listaPeliculas, elemento){
-    let template = ""
-    for (const movieIterado of listaPeliculas) {
-        template += crearPelicula(movieIterado)
-    }
-    if(listaPeliculas.length == 0){
-        template = `<h2 class="font-semibold text-2xl">There are no matching results</h2>`
-    }
-    elemento.innerHTML = template
-}
-
-imprimirPeliculas(movies,main)
+imprimirPeliculas(movies,$main)
 
 /*---------------------Sprint 2--------------------- */
-/*
-<label> genero
-    <input type="checkbox" name="" id="">
- </label>
-*/
 
 
 const $input = document.getElementById('inputBusqueda')
 const $contenedorChecks = document.getElementById('contenedorChecks')
+const arrayGenres = obtenerGenres(movies) //sin repetidos
 
-/* CREAR CHECKBOX:
-
-- conseguir tags
-- quitar repetidos
-- crear checks
-
-*/
-
-const generos = movies.map( movie => movie.genres).flat()  //tags
-
-const generosSinRepetidos = [] //array de no repetidos
-generos.forEach(genero => {
-    if (!generosSinRepetidos.includes(genero)){
-        generosSinRepetidos.push(genero)
-    }
-} )
-
-
-function crearLista(genero){
-    return `<option value=${genero}>
-                    ${genero}     
-            </option>`
-}
-
-const fnReduce = (template, genero) => template + crearLista(genero)
-
-$contenedorChecks.innerHTML = generosSinRepetidos.reduce(fnReduce,"")
+$contenedorChecks.innerHTML = arrayGenres.reduce(fnReduce,"")
 
 $input.addEventListener('input', () => {
     
     const peliculasFiltradasPorNombre = filtrarPeliculasPorNombre( movies, $input.value)
-    const valueChecked = verifiqueChecked()
+    const valueChecked = verifiqueChecked() //agregar if
     const peliculasFiltradasPorGenres = filtrarPeliculasPorGenero(peliculasFiltradasPorNombre, valueChecked)
-    imprimirPeliculas(peliculasFiltradasPorGenres, main)
-} )
+    imprimirPeliculas(peliculasFiltradasPorGenres, $main)
+})
 
-//Quedo algo mal xq no filtra por nombre
 
 $contenedorChecks.addEventListener( 'input', () => {
     const peliculasFiltradasPorNombre = filtrarPeliculasPorNombre( movies, $input.value)
     const valueChecked = verifiqueChecked()
     const peliculasFiltradasPorGenres = filtrarPeliculasPorGenero(peliculasFiltradasPorNombre, valueChecked)
-    imprimirPeliculas(peliculasFiltradasPorGenres, main)
+    imprimirPeliculas(peliculasFiltradasPorGenres, $main)
 
 })
 
 
 
-
-function filtrarPeliculasPorNombre(listaPeliculas, nombre){
-    return listaPeliculas.filter (pelicula => pelicula.title.toLowerCase().startsWith(nombre.toLowerCase()))
-}
-
-function filtrarPeliculasPorGenero(listaPeliculas, seleccionados){
-    if(seleccionados.length == 0) {
-        return listaPeliculas
-    } else {
-        return listaPeliculas.filter( pelicula => seleccionados.some(seleccionado => pelicula.genres.includes (seleccionado)))
-    } 
-}
-
-function verifiqueChecked() {
-    const elementosSeleccionados = [];
-    const opcionesSeleccionadas = document.querySelectorAll('option:checked');
-    
-    opcionesSeleccionadas.forEach(opcion => {
-        elementosSeleccionados.push(opcion.value);
-    });
-
-    return elementosSeleccionados;
-}
 
 
 
